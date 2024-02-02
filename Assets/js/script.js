69af847210a09ee63e7b485f6a92ce6a4537e0a6
Evangelos Koutanis <evans20kom@gmail.com>
2024-02-02 18:16:45 +0000

    fetch query finalized - persistent bug (issue #20 ) to be resolved

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 // Fetch Function Elements Below
 // Define variable for website and API Key - To be defined at later stage
 var APIKey = "SPOONACULAR_API_KEY";
-var queryURL = "https://api.spoonacular.com/recipes/complexSearch" + APIKey;
+var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + APIKey;
 
 // Create fetch call with query parameters - standard method presented in bootcamp. References: © 2024 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
 fetch(queryURL)
